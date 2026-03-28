77a89c85e3e406de365754060671c4afa654958c
Asaad Morman <cyberdojosensai@gmail.com>
2026-03-28 01:08:11 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
